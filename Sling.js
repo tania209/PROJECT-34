d055cfc77b053573b1af68b87b69f3a5d21bf9dd
tania209 <70532882+tania209@users.noreply.github.com>
2020-12-21 07:45:22 -0800

    Add files via upload

---
+++ Sling.js
@@ -0,0 +1,26 @@
+class Sling {
+  constructor(bodyA, pointB) {
+    var options = {
+      bodyA: bodyA,
+      pointB: pointB,
+      stiffness: 1,
+      angularStiffness: 1,
+      length: 220
+    };
+    this.pointB = pointB;
+    this.sling = Constraint.create(options);
+    World.add(world, this.sling);
+  }
+
+  display() {
+    if (this.sling.bodyA) {
+      var pointA = this.sling.bodyA.position;
+      var pointB = this.pointB;
+      push();
+      strokeWeight(3.5);
+      stroke("#fff");
+      line(pointA.x, pointA.y, pointB.x, pointB.y);
+      pop();
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,46 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+var world, engine, canvas;
+var pendulum1, pendulum2, pendulum3, pendulum4, pendulum5;
+var sling1, sling2, sling3, sling4, sling5;
+
+function setup() {
+  canvas = createCanvas(windowWidth / 2, windowHeight / 1.5);
+  engine = Engine.create();
+  world = engine.world;
+
+  pendulum1 = new Pendulum(340, 450, "pink");
+  pendulum2 = new Pendulum(400, 450, "purple");
+  pendulum3 = new Pendulum(460, 450, "orange");
+  pendulum4 = new Pendulum(520, 450, "blue");
+  pendulum5 = new Pendulum(580, 450, "yellow");
+
+  sling1 = new Sling(pendulum1.body, { x: 340, y: 200 });
+  sling2 = new Sling(pendulum2.body, { x: 400, y: 200 });
+  sling3 = new Sling(pendulum3.body, { x: 460, y: 200 });
+  sling4 = new Sling(pendulum4.body, { x: 520, y: 200 });
+  sling5 = new Sling(pendulum5.body, { x: 580, y: 200 });
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+  pendulum1.display();
+  pendulum2.display();
+  pendulum3.display();
+  pendulum4.display();
+  pendulum5.display();
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+  sling5.display();
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(pendulum1.body, { x: mouseX, y: mouseY });
+}
